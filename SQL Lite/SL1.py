f2061a1dda14cbf1f92c75f560d77423f520f87b
AbhijeetShriv <abhijeetshrivas1@outlook.com>
2026-04-01 23:40:22 +0530

    Update SL1.py

--- SQL Lite/SL1.py
+++ SQL Lite/SL1.py
@@ -1,8 +1,18 @@
-import pandas as pd
+# SELECT CODES AND THAN [CTRL + /].It Helps To Comment OR Uncomment the code us
+import sqlite3
 
-df = pd.DataFrame({
-    "Name": ["Abhi", "Raj"],
-    "Marks": [81, 10]
-})
+connector=sqlite3.connect("ONE.db",isolation_level=None)
+cursoor=connector.cursor()
 
-print(df)
+cursoor.execute("CREATE TABLE Tipper(ID INT, NAME TEXT)")
+uid=int(input("Enter ID: "))
+nam=input("Enter Name: ")
+cursoor.execute("INSERT INTO Tipper(ID,NAME) VALUES(?,?)",(uid,nam))
+cursoor.execute("SELECT * FROM Tipper")
+rows=cursoor.fetchall()
+print("ID\t NAME")
+print("-" * 99)
+for row in rows:
+    print(row[0],"\t",row[1])
+cursoor.execute("UPDATE Tipper SET NAME = ? WHERE ID = ?",(nam,uid))
+cursoor.execute("DELETE FROM Tipper where ID = ?",(uid,))
