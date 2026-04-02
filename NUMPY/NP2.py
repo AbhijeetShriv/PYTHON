6fa9af966b567b706ca84c7e83408e415c1dcf43
AbhijeetShriv <abhijeetshrivas1@outlook.com>
2026-04-02 16:58:52 +0530

    Add files via upload

---
+++ NUMPY/NP2.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# A) Arithmetic Operations
+# Addition, Subtraction, Multiplication
+a = np.array([10, 20, 30])
+b = np.array([1, 2, 3])
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+#
+# # Scalar Operations
+print(a + 5)
+print(a * 2)
