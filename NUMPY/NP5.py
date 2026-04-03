6f17e70b28424c79f2f2621d57d49e7495a8d1c4
AbhijeetShriv <abhijeetshrivas1@outlook.com>
2026-04-03 14:16:58 +0530

    Update NP5.py

--- NUMPY/NP5.py
+++ NUMPY/NP5.py
@@ -11,3 +11,16 @@ array1 = kp.array([[1, 2, 3, 4],
                    [5, 6, 7, 8],
                    [9, 10, 11, 12]])
 print(array1[1:2, 2:4])
+
+array2 = kp.arange(1, 7)
+print(array2)
+array3 = array2.reshape(2, 3)
+print(array3)
+print(array3.flatten())
+
+array4 = kp.array([[1, 2, 3], [4, 5, 6]])
+
+print(array4.shape)
+print(array4.ndim)
+print(array4.size)
+print(array4.dtype)
